e6c419771d0c32808037f72bb4b33658e3deec57
Dzoter <Andry94@list.ru>
2022-04-21 05:48:37 +0400

    добавил рабочий фильтр за исключением периода (т.к. все задания больше 24 часов, думаю этот фрагмент кода подключить позже)

--- controllers/TasksController.php
+++ controllers/TasksController.php
@@ -2,14 +2,41 @@
 
 namespace app\controllers;
 
+use app\models\forms\TaskFilterForm;
 use app\models\Tasks;
+use Yii;
 
 class TasksController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-        $query = Tasks::find()->where(['executor_id'=>null])->limit(3)->all();
-        return $this->render('index', ['taskInfo' => $query]);
+        TaskFilterForm::getCategories();
+        $taskFilterForm = new TaskFilterForm();
+        $taskFilterForm->load(Yii::$app->request->post());
+
+        if (Yii::$app->request->post()){
+            if ($taskFilterForm->isNoExecutor) {
+                $taskFilterForm->isNoExecutor = 'IS NULL';
+            } else {
+                $taskFilterForm->isNoExecutor = 'IS NOT NULL';
+            }
+
+            if ($taskFilterForm->isRemote) {
+                $taskFilterForm->isRemote = 'IS NOT NULL';
+            } else {
+                $taskFilterForm->isRemote = '= 689';
+            }
+        }
+
+
+        $query = Tasks::find()->where("executor_id $taskFilterForm->isNoExecutor")
+            ->andWhere([
+                'category_id' => $taskFilterForm->categoryIds,
+            ])
+            ->andWhere("city_id $taskFilterForm->isRemote")
+            ->all();
+
+        return $this->render('index', ['taskInfo' => $query, 'taskFilterForm' => $taskFilterForm]);
     }
 
 
--- fixtures/data/TaskFiles.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'TaskFiles0' => [
-        'task_id' => 2,
-        'file_id' => 1,
-    ],
-
-];
