aaf8d044c96cb59e7a5c03bd77b25e64944899d1
Dzoter <Andry94@list.ru>
2022-06-21 22:12:53 +0400

    подключил библиотеку guzzle

---
+++ controllers/ApiController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\web\Controller;
+use GuzzleHttp\Client;
+
+class ApiController extends Controller
+{
+    public function actionIndex()
+    {
+
+    }
+
+
+
+}
